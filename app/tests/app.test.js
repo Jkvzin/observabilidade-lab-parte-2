efae1ffbff36950529a5c671f66164c0ec913102
João Victor <joaovictorbc1457@gmail.com>
2026-06-22 21:21:01 -0300

    refactor: TechStore -> JJ Eletronicos

--- app/tests/app.test.js
+++ app/tests/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('API TechStore - Ecommerce', () => {
+describe('API JJ Eletronicos - Ecommerce', () => {
     // ==================== HEALTH ====================
     describe('GET /', () => {
         test('deve redirecionar para o dashboard HTML (302)', async () => {
